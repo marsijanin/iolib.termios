684e126c92d29dda092016c7a8736dcb5fe1d77e
Nikolay V. Razbegaev <marsijanin@gmail.com>
2009-09-18 22:44:50 +0400

    defsyscall for posix termios api

--- ffi-termios-functions-unix.lisp
+++ ffi-termios-functions-unix.lisp
@@ -4,5 +4,43 @@
 (in-package :iolib.termios)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defsyscall (%cfgetispeed "cfgetispeed") :speed
-  (termios :pointer))			;const struct termios *
+  (termios :pointer))			; const struct termios *
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defsyscall (%cfgetospeed "cfgetospeed") :speed
+  (termios :pointer))			; const struct termios *
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defsyscall (%cfsetispeed "cfsetispeed") :int
+  (termios :pointer)			; struct termios *
+  (speed   :speed))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defsyscall (%cfsetospeed "cfsetospeed") :int
+  (termios :pointer) 			; struct termios *	
+  (speed   :speed))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defsyscall (%tcdrain "tcdrain") :int
+  (fd :int))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defsyscall (%tcflow "tcflow")   :int
+  (fd     :int)
+  (action :int))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defsyscall (%tcflush "tcflush") :int
+  (queue-selector :int)
+  (fd             :int))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defsyscall (%tcgetattr "tcgetattr") :int
+  (fd      :int)
+  (termios :pointer)) 			; const struct termios *
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defsyscall (%tcgetsid "tcgetsid") pid-t
+  (fd :int))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defsyscall (%tcsendbreak "tcsendbreak") :int
+  (fd       :int)
+  (duration :int))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defsyscall (%tcsetattr "tcsetattr") :int
+  (fd             :int)
+  (optional-ation :int)
+  (termios        :pointer))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
