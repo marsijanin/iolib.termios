c95714cc96b108bcd0d60cce725cf2a377ac1aee
Nikolay V. Razbegaev <marsijanin@gmail.com>
2009-09-18 23:15:00 +0400

    using :handle for termios function working with fd

--- ffi-termios-functions-unix.lisp
+++ ffi-termios-functions-unix.lisp
@@ -20,26 +20,26 @@
 (defsyscall (%tcdrain "tcdrain") :int
   (fd :int))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defsyscall (%tcflow "tcflow")   :int
+(defsyscall (%tcflow "tcflow")   (:int :restart t :handle fd)
   (fd     :int)
   (action :int))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defsyscall (%tcflush "tcflush") :int
+(defsyscall (%tcflush "tcflush") (:int :restart t :handle fd)
   (queue-selector :int)
   (fd             :int))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defsyscall (%tcgetattr "tcgetattr") :int
+(defsyscall (%tcgetattr "tcgetattr") (:int :restart t :handle fd)
   (fd      :int)
   (termios :pointer)) 			; const struct termios *
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defsyscall (%tcgetsid "tcgetsid") pid-t
   (fd :int))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defsyscall (%tcsendbreak "tcsendbreak") :int
+(defsyscall (%tcsendbreak "tcsendbreak") (:int :restart t :handle fd)
   (fd       :int)
   (duration :int))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defsyscall (%tcsetattr "tcsetattr") :int
+(defsyscall (%tcsetattr "tcsetattr") (:int :restart t :handle fd)
   (fd             :int)
   (optional-ation :int)
   (termios        :pointer))
