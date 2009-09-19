9d4c60be3cb0bcd60ec1c2db52dbd2abcaad04ec
Nikolay V. Razbegaev <marsijanin@gmail.com>
2009-09-19 12:43:28 +0400

    + set-termios-option

--- wrapers.lisp
+++ wrapers.lisp
@@ -10,7 +10,7 @@
 ;; don't using defconstant case sbcl singnal constant redefining error
 ;; with and w/o eval-when case results are not eql
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defparameter +cflags+ '(cbaud exta extb csize cs5 cs6 cs7 cs8
+(defparameter *cflags* '(cbaud exta extb csize cs5 cs6 cs7 cs8
 			 cstopb cread parenb parodd hupcl clocal
 			 loblk crtscts)   
   "Termios cflag filed constant")
@@ -45,4 +45,50 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; </ Termios flags by fields >
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun set-termios-option (termios flag-or-control-character &optional value)
+  "Setup termios flag or control character. If `flag-or-control-character'
+   is one of the termios flags (i.e. icanon) and no value is specified
+   flag in corresponding termios field is reseted, otherwise seted.
+   If  `flag-or-control-character' is control character corresponding
+   value in termios cc field is seted to the `value' parameter.
 
+   Reset icanon flag: (set-termios-option term 'icanon)
+   Set   icanon flag: (set-termios-option term 'icanon t)
+   Set vtime value to 0: (set-termios-option 'vtime 0)
+  "
+  (labels ((set-flag (flag field)
+	     (setf (foreign-slot-value termios 'termios field)
+		   (logior flag (foreign-slot-value termios 'termios field))))
+	   (reset-flag (flag field)
+	     (setf (foreign-slot-value termios 'termios field)
+		   (logandc2 flag (foreign-slot-value termios 'termios field))))
+	   (set-control-character (character value)
+	     (setf (mem-aref (foreign-slot-pointer termios 'termios 'control-chars)
+			     :cc character)
+		   value)))
+    (let ((fvalue (symbol-value flag-or-control-character)))
+      (cond ((member flag-or-control-character *cflags*)
+	     (if value
+		 (set-flag fvalue 'cflag)
+		 (reset-flag fvalue 'cflag)))
+	    ((member flag-or-control-character *lflags*)
+	     (if value
+		 (set-flag fvalue 'lflag)
+		 (reset-flag fvalue 'lflag)))
+	    ((member flag-or-control-character *iflags*)
+	     (if value
+		 (set-flag fvalue 'iflag)
+		 (reset-flag fvalue 'iflag)))
+	    ((member flag-or-control-character *oflags*)
+	     (if value
+		 (set-flag fvalue 'oflag)
+		 (reset-flag fvalue 'oflag)))
+	    ((member flag-or-control-character *control-characters*)
+	     (if value
+		 (set-control-character fvalue value)
+		 (error "You should specify control character value")))
+	    (t (error "Unknown termios flag or control character ~a"
+		      flag-or-control-character))))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+		 
